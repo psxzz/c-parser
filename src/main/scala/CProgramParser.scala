3736f254fe46f0c2b496fb2a0b836c6d5774bde9
psxzz <m.martusevich@yandex.ru>
2023-11-26 00:16:09 +0300

    Add: expression token

--- src/main/scala/CProgramParser.scala
+++ src/main/scala/CProgramParser.scala
@@ -2,16 +2,15 @@ import scala.util.parsing.combinator._
 
 class CProgramParser extends RegexParsers {
     def identifier : Parser[IdentifierTk] = """[a-zA-Z_][0-9a-zA-Z_]*""".r ^^ {id => IdentifierTk(id.toString)}
-    def number : Parser[Int] = """(0|[1-9]\d*)""".r ^^ {_.toInt}
-    def boolean : Parser[Boolean] = """(true|false)""".r ^^ {_.toBoolean}
+    def number : Parser[IntTk] = """(0|[1-9]\d*)""".r ^^ { v => IntTk(v.toInt) }
+    def boolean : Parser[BoolTk] = """(true|false)""".r ^^ { v => BoolTk(v.toBoolean) }
 
     def typedef : Parser[TypeTk] = """(int|bool)""".r ^^ {
         t => TypeTk(t)
     }
 
     def varDecl : Parser[VariableDeclTk] = typedef ~ identifier ~ opt("=".r ~> (number | boolean)) <~ ";".r ^^ {
-        case t ~ id ~ Some(value) => VariableDeclTk(t, id, value.toString)
-        case t ~ id ~ None => VariableDeclTk(t, id, "")
+        case t ~ id ~ exp => VariableDeclTk(t, id, exp)
     }
 
     def program : Parser[ProgramTk] = rep(varDecl) ^^ {
--- src/main/scala/Token.scala
+++ src/main/scala/Token.scala
@@ -7,10 +7,15 @@ case class ProgramTk() extends Token {
 }
 
 
-case class VariableDeclTk(varType : TypeTk, id : IdentifierTk, value : String) extends Token
+case class VariableDeclTk(varType : TypeTk, id : IdentifierTk, value : Option[Exp]) extends Token
 
 case class IdentifierTk(str : String) extends Token
 
 case class TypeTk(t : String) extends Token
 
 case class FunctionDeclTk(retType : TypeTk, id : IdentifierTk) extends Token
+
+abstract class Exp extends Token
+case class IntTk(value : Int) extends Exp
+case class BoolTk(value : Boolean) extends Exp
+

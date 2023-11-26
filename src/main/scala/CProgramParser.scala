1c265e67fcfd30120731b4c07ef377f1974f6e46
psxzz <m.martusevich@yandex.ru>
2023-11-26 15:01:17 +0300

    Add: expressions and statements

--- src/main/scala/CProgramParser.scala
+++ src/main/scala/CProgramParser.scala
@@ -1,25 +1,51 @@
 import scala.util.parsing.combinator._
 
 class CProgramParser extends RegexParsers {
-    def identifier : Parser[IdentifierTk] = """[a-zA-Z_][0-9a-zA-Z_]*""".r ^^ {id => IdentifierTk(id.toString)}
+    // Tokens
+    def identifier : Parser[Identifier] = """[a-zA-Z_][0-9a-zA-Z_]*""".r ^^ {id => Identifier(id.toString)}
     def number : Parser[IntTk] = """(0|[1-9]\d*)""".r ^^ { v => IntTk(v.toInt) }
-    def boolean : Parser[BoolTk] = """(true|false)""".r ^^ { v => BoolTk(v.toBoolean) }
+    def bool : Parser[BoolTk] = """(true|false)""".r ^^ { v => BoolTk(v.toBoolean) }
+    def typedef : Parser[Type] = """(int|bool)""".r ^^ { t => Type(t) }
 
-    def typedef : Parser[TypeTk] = """(int|bool)""".r ^^ {
-        t => TypeTk(t)
-    }
-
-    def varDecl : Parser[VariableDeclTk] = typedef ~ identifier ~ opt("=".r ~> (number | boolean)) <~ ";".r ^^ {
+    // Declarations
+    def varDecl : Parser[VariableDeclTk] = typedef ~ identifier ~ opt("=" ~> (arithExp | boolExp)) <~ ";" ^^ {
         case t ~ id ~ exp => VariableDeclTk(t, id, exp)
     }
+    def funcDecl : Parser[FunctionDeclTk] = typedef ~ identifier ~ "()" ~ compound ^^ {
+        case t ~ id ~ _ ~ body => FunctionDeclTk(t, id, body)
+    }
 
-    def program : Parser[ProgramTk] = rep(varDecl) ^^ {
-        case vs => {
+    // Statements
+    def statement : Parser[Stmt] = compound | assign ^^ { _ => CompoundStmt() }
+    def compound : Parser[CompoundStmt] = "{" ~ rep(statement) ~ "}" ^^ { _ => CompoundStmt() }
+    def assign : Parser[AssignStmt] = identifier ~ "=" ~ expression ^^ {
+        case id ~ _ ~ exp => AssignStmt(id, exp)
+    }
+
+    // Expressions
+    def expression : Parser[Exp] = identifierExp | arithExp | boolExp ^^ {}
+    def identifierExp : Parser[IdentifierExp] = identifier ^^ {
+        case Identifier(v) => IdentifierExp(v)
+    }
+    def arithExp : Parser[ArithExp] = number ^^ {
+        case IntTk(v) => ArithExp(v)
+    }
+    def boolExp : Parser[BoolExp] = bool ^^ {
+        case BoolTk(v) => BoolExp(v)
+    }
+
+    // Arithmetical operations
+
+    // Boolean operations
+
+    // Entry point
+    def program : Parser[ProgramTk] = rep(varDecl | funcDecl) ^^ {
+        case vs =>
             val prog = ProgramTk()
-            for (v <- vs) {
-                prog.globalVarDecls = v :: prog.globalVarDecls
+            for (v <- vs) v match {
+                case VariableDeclTk(t, id, value) => {prog.globalVarDecls = VariableDeclTk(t, id, value) :: prog.globalVarDecls }
+                case FunctionDeclTk(t, id, body) => {prog.globalFuncDecls = FunctionDeclTk(t, id, body) :: prog.globalFuncDecls}
             }
             prog
-        }
     }
 }
--- src/main/scala/Token.scala
+++ src/main/scala/Token.scala
@@ -2,20 +2,36 @@ sealed trait Token
 
 case class ProgramTk() extends Token {
     var globalVarDecls : List[VariableDeclTk] = Nil
+    var globalFuncDecls : List[FunctionDeclTk] = Nil
 
-    override def toString: String = globalVarDecls.toString()
+    override def toString: String = globalVarDecls.toString() + "\n" + globalFuncDecls.toString()
 }
 
 
-case class VariableDeclTk(varType : TypeTk, id : IdentifierTk, value : Option[Exp]) extends Token
+abstract class Declaration extends Token
 
-case class IdentifierTk(str : String) extends Token
+case class VariableDeclTk(varType : Type, id : Identifier, value : Option[Exp]) extends Declaration
+case class FunctionDeclTk(retType : Type, id : Identifier, body : CompoundStmt) extends Declaration
 
-case class TypeTk(t : String) extends Token
+case class Identifier(str : String) extends Token
 
-case class FunctionDeclTk(retType : TypeTk, id : IdentifierTk) extends Token
+case class Type(t : String) extends Token
+
+case class IntTk(value : Int) extends Token
+case class BoolTk(value : Boolean) extends Token
+
 
 abstract class Exp extends Token
-case class IntTk(value : Int) extends Exp
-case class BoolTk(value : Boolean) extends Exp
+case class ArithExp(value : Int) extends Exp
+case class BoolExp(value : Boolean) extends Exp
+case class IdentifierExp(id: String) extends Exp
 
+abstract class Stmt extends Token
+case class CompoundStmt() extends Stmt
+case class AssignStmt(id: Identifier, exp: Exp) extends Stmt
+
+abstract class Op extends Token
+case class AddOp(l :Int, r :Int) extends Op
+case class SubOp(l :Int, r :Int) extends Op
+case class MulOp(l :Int, r :Int) extends Op
+case class DivOp(l :Int, r :Int) extends Op

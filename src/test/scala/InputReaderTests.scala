2e0f372cb1abbf259f1e3b4c59ef0ecefd058d25
psxzz <m.martusevich@yandex.ru>
2023-11-28 23:54:16 +0300

    Fix tests

--- src/test/scala/InputReaderTests.scala
+++ src/test/scala/InputReaderTests.scala
@@ -2,8 +2,8 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class InputReaderTests extends AnyFunSuite {
     test("InputReader reads from file") {
-        val result = new FileInputReader("./src/test/samples/simple_input.c").read()
-        val expected = s"int main() {\r\n    printf(\"Hello world!\");\r\n    return 0;\r\n}"
+        val result = new FileInputReader("./src/test/samples/simple_input.c").read().replace("\r\n", "\n")
+        val expected = s"int main() {\n    printf(\"Hello world!\");\n    return 0;\n}"
         assert(result == expected)
     }
 }
